537c0286f859c892c5f5448e8722ee82c6080b50
MChristensson <magnus.christensson@hotmail.se>
2017-12-14 22:18:13 +0100

    Baseline mvn files

---
+++ core/src/main/java/org/mac/util/unittestrunner/App.java
@@ -0,0 +1,13 @@
+package org.mac.util.unittestrunner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
